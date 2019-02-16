99737a6c79328816207d132b82118adc55c4dc79
Tracy Ngo <tngo1206@gmail.com>
2019-02-16 02:13:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_hash.each do |season, holiday|
+    holiday_keys = []
+    holiday.each do |holiday, supplies|
+      if holiday.values == "BBQ"
   
 end
 end
